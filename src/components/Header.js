9eeefb9518bdd7a9dfa3634194033f170a19ba33
teasolves <pisipisi@gmail.com>
2022-10-06 10:18:54 +0300

    search cleared

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Navbar,Container } from 'react-bootstrap'
+
+const navbarStyle={
+    backgroundColor: 'lightblue'
+};
+
+const Header = (props)=> {
+    return(
+
+        <Navbar style={navbarStyle} variant="light">
+            <Container>
+                <Navbar.Brand href="/">{props.title}</Navbar.Brand>
+            </Container>
+        </Navbar>
+
+
+    )
+};
+
+export default Header;
---
+++ src/components/Search.js
@@ -0,0 +1,35 @@
+import react from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Button, Form } from 'react-bootstrap';
+
+const Search = ({word,setWord,handleSubmit}) => {
+
+    return (
+        <Container className='mt-4'>
+            <Row className="justify-content-center">
+                <Col xs={12} md={8} lg={6}>
+                    <Form onSubmit={handleSubmit}>    
+                        <Form.Row>
+                            <Col xs={9}>
+                                <Form.Control
+                                    type='text'
+                                    value={word}
+                                    onChange={(e)=> setWord(e.target.value)}
+                                    placeholder="Search for image...." 
+                                />
+                            </Col>
+                            <Col>
+                                <Button variant="primary" type="submit">Search</Button>
+                            </Col>
+                        </Form.Row>
+                    </Form>
+                        
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default Search;
